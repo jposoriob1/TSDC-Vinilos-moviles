7df983c2c477a3d3a8153d13d1b3511d16dc9213
Carlos Bayona <c.bayona@uniandes.edu.co>
2025-04-13 17:02:04 -0500

    Updated package folder

--- app/src/main/java/com/example/tsdc/MainActivity.kt
+++ app/src/main/java/com/example/tsdc/MainActivity.kt
@@ -1,11 +1,9 @@
-package com.example.firstproject
+package com.example.tsdc
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -13,8 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
