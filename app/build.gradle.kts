c0a0eb75a008d6bdb4d3893f56f1ae687192aa5b
James <jaimea111@gmail.com>
2025-04-13 21:20:51 -0500

    Solving imcompatibility issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.tsdc"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.tsdc"
         minSdk = 22
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
