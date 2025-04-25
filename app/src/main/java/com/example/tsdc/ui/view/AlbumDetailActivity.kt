4c07a81604bd1fbf18d028884041068117db7471
jposoriob1 <jp.osoriob1@uniandes.edu.co>
2025-04-24 23:27:46 -0500

    cambio en baseURL

--- app/src/main/java/com/example/tsdc/ui/view/AlbumDetailActivity.kt
+++ app/src/main/java/com/example/tsdc/ui/view/AlbumDetailActivity.kt
@@ -22,7 +22,7 @@ class AlbumDetailActivity : ComponentActivity() {
             TSDCTheme {
 
                 val retrofit = Retrofit.Builder()
-                    .baseUrl("http://192.168.1.109:3000/")
+                    .baseUrl("http://10.0.2.2:3000/")
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
 
