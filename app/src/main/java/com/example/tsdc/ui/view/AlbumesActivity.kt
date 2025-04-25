af6d5d9a3ce647eb6344f13313156560443eb11c
jposoriob1 <jp.osoriob1@uniandes.edu.co>
2025-04-24 23:32:34 -0500

    hotfix baserUrl general

--- app/src/main/java/com/example/tsdc/ui/view/AlbumesActivity.kt
+++ app/src/main/java/com/example/tsdc/ui/view/AlbumesActivity.kt
@@ -21,7 +21,7 @@ class AlbumesActivity : ComponentActivity() {
         setContent {
             TSDCTheme {
                 val retrofit = Retrofit.Builder()
-                    .baseUrl("http://192.168.1.109:3000/")
+                    .baseUrl("http://10.0.2.2:3000/")
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
 
