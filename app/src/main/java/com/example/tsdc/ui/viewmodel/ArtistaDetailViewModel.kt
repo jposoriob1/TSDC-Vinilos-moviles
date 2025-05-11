3d21c83b7b4e0c54a582abd4f11578556d701caf
Juan Pablo Osorio Beltran <jp.osoriob1@uniandes.edu.co>
2025-05-10 20:03:12 -0500

    Micro-optimizaciones con corrutinas, implementacion de cache en memoria (#78)

--- app/src/main/java/com/example/tsdc/ui/viewmodel/ArtistaDetailViewModel.kt
+++ app/src/main/java/com/example/tsdc/ui/viewmodel/ArtistaDetailViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.viewModelScope
 import com.example.tsdc.data.repository.ArtistasRepository
 import com.example.tsdc.data.model.TipoArtistaDto
 import com.example.tsdc.ui.state.ArtistaDetailUiState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ArtistaDetailViewModel(
     private val repository: ArtistasRepository
@@ -18,19 +20,22 @@ class ArtistaDetailViewModel(
 
     fun cargarDetalle(artistaId: Int, tipo: TipoArtistaDto) {
         viewModelScope.launch {
+            _uiState.value = ArtistaDetailUiState.Loading
             try {
-                _uiState.value = ArtistaDetailUiState.Loading
-                _uiState.value = when (tipo) {
-                    TipoArtistaDto.MUSICO -> {
-                        val musico = repository.getMusicoById(artistaId)
-                        ArtistaDetailUiState.SuccessMusico(musico)
-                    }
+                val resultado = withContext(Dispatchers.IO) {
+                    when (tipo) {
+                        TipoArtistaDto.MUSICO -> {
+                            val musico = repository.getMusicoById(artistaId)
+                            ArtistaDetailUiState.SuccessMusico(musico)
+                        }
 
-                    TipoArtistaDto.BANDA -> {
-                        val banda = repository.getBandaById(artistaId)
-                        ArtistaDetailUiState.SuccessBanda(banda)
+                        TipoArtistaDto.BANDA -> {
+                            val banda = repository.getBandaById(artistaId)
+                            ArtistaDetailUiState.SuccessBanda(banda)
+                        }
                     }
                 }
+                _uiState.value = resultado
             } catch (e: Exception) {
                 _uiState.value = ArtistaDetailUiState.Error(e.message ?: "Error desconocido")
             }
